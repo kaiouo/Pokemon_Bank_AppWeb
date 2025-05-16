67aeecb6726742dc42ef052ffe65000f9c557a8b
DavidRFCoto <davidrobertoferrercoto@gmail.com>
2025-05-16 13:15:01 -0600

    actualizacion de saldo

--- comprobante.js
+++ comprobante.js
@@ -1,4 +1,4 @@
-    document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("formDeposito");
 
     form.addEventListener("submit", function(e) {
